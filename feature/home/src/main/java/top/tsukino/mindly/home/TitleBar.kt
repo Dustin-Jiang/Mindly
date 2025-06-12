fac6a05ea893d814ce4bf7cd49e61721e0702938
Dustin Jiang <dustinjiang@outlook.com>
2025-06-12 17:45:20 +0800

    🚮 移除无用依赖

--- feature/home/src/main/java/top/tsukino/mindly/home/TitleBar.kt
+++ feature/home/src/main/java/top/tsukino/mindly/home/TitleBar.kt
@@ -1,11 +1,6 @@
 package top.tsukino.mindly.home
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
